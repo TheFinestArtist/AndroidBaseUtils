fb97f535070c993f6f3cd209e064f156c0e80ff5
Leonardo Taehwan Kim <leonardo@thefinestartist.com>
2016-03-15 07:37:28 +0900

    Update Extra annotation class

--- annotations/src/main/java/com/thefinestartist/annotations/Extra.java
+++ annotations/src/main/java/com/thefinestartist/annotations/Extra.java
@@ -1,8 +1,19 @@
 package com.thefinestartist.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
 /**
- * Created by TheFinestArtist on 2/7/16.
+ * Bind a field to the bundle data for the specific key.
+ * <pre><code>
+ * {@literal @}Extra(EXTRA_TITLE) String title;
+ * </code></pre>
  */
+@Retention(CLASS)
+@Target(FIELD)
 public @interface Extra {
     String value() default "";
 }

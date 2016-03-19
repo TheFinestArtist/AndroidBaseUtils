4032a4ddac0a2bd65620a9719c2bb81a731fc758
Leonardo Taehwan Kim <leonardo@thefinestartist.com>
2016-03-19 12:09:01 +0900

    Update art

--- sample/src/main/java/com/thefinestartist/utils/sample/MainActivity.java
+++ sample/src/main/java/com/thefinestartist/utils/sample/MainActivity.java
@@ -8,19 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.orhanobut.logger.Logger;
 import com.thefinestartist.helpers.log.LogHelper;
 import com.thefinestartist.utils.log.LogUtil;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * Created by TheFinestArtist on 1/30/16.
  */
 public class MainActivity extends AppCompatActivity {
 
-    LogHelper logHelper = new LogHelper(Fragment1.class).methodCount(3).showThreadInfo(true);
+    LogHelper logHelper = new LogHelper(Fragment1.class).showThreadInfo(true);
 
     EditText editText;
     Button button;
@@ -69,57 +65,65 @@ public class MainActivity extends AppCompatActivity {
 //        VibratorUtil.eee();
 //        KeyboardUtil.showImmediately(editText);
 
-        logHelper.v("Hey");
-        logHelper.v("Hello");
-        try {
-            JSONObject jsonObject = new JSONObject("caller\":\"getPoiById\",\"results\":{\"indexForPhone\":0,\"indexForEmail\":\"NULL\",\"indexForHomePage\":\"NULL\",\"indexForComment\":\"NULL\",\"phone\":\"05137-930 68\",\"cleanPhone\":\"0513793068\",\"internetAccess\":\"2\",\"overnightStay\":\"2\",\"wasteDisposal\":\"2\",\"toilet\":\"2\",\"electricity\":\"2\",\"cran\":\"2\",\"slipway\":\"2\",\"camping\":\"2\",\"freshWater\":\"2\",\"fieldNamesWithValue\":[\"phone\"],\"fieldNameTranslations\":[\"Telefon\"],\"id\":\"1470\",\"name\":\"Marina Rasche Werft GmbH & Co. KG\",\"latitude\":\"52.3956107286487\",\"longitude\":\"9.56583023071289\"}}");
-            logHelper.e(jsonObject);
-        } catch (JSONException e) {
-            LogUtil.e(e);
-            logHelper.e(e);
-        }
-
-        logHelper.json("{\"caller\":\"getPoiById\",\"results\":{\"indexForPhone\":0,\"indexForEmail\":\"NULL\",\"indexForHomePage\":\"NULL\",\"indexForComment\":\"NULL\",\"phone\":\"05137-930 68\",\"cleanPhone\":\"0513793068\",\"internetAccess\":\"2\",\"overnightStay\":\"2\",\"wasteDisposal\":\"2\",\"toilet\":\"2\",\"electricity\":\"2\",\"cran\":\"2\",\"slipway\":\"2\",\"camping\":\"2\",\"freshWater\":\"2\",\"fieldNamesWithValue\":[\"phone\"],\"fieldNameTranslations\":[\"Telefon\"],\"id\":\"1470\",\"name\":\"Marina Rasche Werft GmbH & Co. KG\",\"latitude\":\"52.3956107286487\",\"longitude\":\"9.56583023071289\"}}");
-
-        logHelper.xml("<set xmlns:android=\"http://schemas.android.com/apk/res/android\">\n" +
-                "    <!-- Before rotating, immediately set the alpha to 0. -->\n" +
-                "    <objectAnimator\n" +
-                "        android:valueFrom=\"1.0\"\n" +
-                "        android:valueTo=\"0.0\"\n" +
-                "        android:propertyName=\"alpha\"\n" +
-                "        android:duration=\"0\" />\n" +
-                "\n" +
-                "    <!-- Rotate. -->\n" +
-                "    <objectAnimator\n" +
-                "        android:valueFrom=\"-180\"\n" +
-                "        android:valueTo=\"0\"\n" +
-                "        android:propertyName=\"rotationY\"\n" +
-                "        android:interpolator=\"@android:anim/accelerate_decelerate_interpolator\"\n" +
-                "        android:duration=\"300\" />\n" +
-                "\n" +
-                "    <!-- Half-way through the rotation (see startOffset), set the alpha to 1. -->\n" +
-                "    <objectAnimator\n" +
-                "        android:valueFrom=\"0.0\"\n" +
-                "        android:valueTo=\"1.0\"\n" +
-                "        android:propertyName=\"alpha\"\n" +
-                "        android:startOffset=\"150\"\n" +
-                "        android:duration=\"1\" />\n" +
-                "</set>");
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                logHelper.e("Hey");
-            }
-        });
-        thread.start();
-
-        LogUtil.methodCount(3).json("{\"name\":\"Leonardo Taehwan Kim\",\"email\":\"leonardo@thefinestartist.com\"}");
-        LogUtil.e("Hello World");
-
+//        logHelper.v("Hey");
+//        logHelper.v("Hello");
+//        try {
+//            JSONObject jsonObject = new JSONObject("caller\":\"getPoiById\",\"results\":{\"indexForPhone\":0,\"indexForEmail\":\"NULL\",\"indexForHomePage\":\"NULL\",\"indexForComment\":\"NULL\",\"phone\":\"05137-930 68\",\"cleanPhone\":\"0513793068\",\"internetAccess\":\"2\",\"overnightStay\":\"2\",\"wasteDisposal\":\"2\",\"toilet\":\"2\",\"electricity\":\"2\",\"cran\":\"2\",\"slipway\":\"2\",\"camping\":\"2\",\"freshWater\":\"2\",\"fieldNamesWithValue\":[\"phone\"],\"fieldNameTranslations\":[\"Telefon\"],\"id\":\"1470\",\"name\":\"Marina Rasche Werft GmbH & Co. KG\",\"latitude\":\"52.3956107286487\",\"longitude\":\"9.56583023071289\"}}");
+//            logHelper.e(jsonObject);
+//        } catch (JSONException e) {
+//            LogUtil.e(e);
+//            logHelper.e(e);
+//        }
+//
+//        logHelper.json("{\"caller\":\"getPoiById\",\"results\":{\"indexForPhone\":0,\"indexForEmail\":\"NULL\",\"indexForHomePage\":\"NULL\",\"indexForComment\":\"NULL\",\"phone\":\"05137-930 68\",\"cleanPhone\":\"0513793068\",\"internetAccess\":\"2\",\"overnightStay\":\"2\",\"wasteDisposal\":\"2\",\"toilet\":\"2\",\"electricity\":\"2\",\"cran\":\"2\",\"slipway\":\"2\",\"camping\":\"2\",\"freshWater\":\"2\",\"fieldNamesWithValue\":[\"phone\"],\"fieldNameTranslations\":[\"Telefon\"],\"id\":\"1470\",\"name\":\"Marina Rasche Werft GmbH & Co. KG\",\"latitude\":\"52.3956107286487\",\"longitude\":\"9.56583023071289\"}}");
+//
+//        logHelper.xml("<set xmlns:android=\"http://schemas.android.com/apk/res/android\">\n" +
+//                "    <!-- Before rotating, immediately set the alpha to 0. -->\n" +
+//                "    <objectAnimator\n" +
+//                "        android:valueFrom=\"1.0\"\n" +
+//                "        android:valueTo=\"0.0\"\n" +
+//                "        android:propertyName=\"alpha\"\n" +
+//                "        android:duration=\"0\" />\n" +
+//                "\n" +
+//                "    <!-- Rotate. -->\n" +
+//                "    <objectAnimator\n" +
+//                "        android:valueFrom=\"-180\"\n" +
+//                "        android:valueTo=\"0\"\n" +
+//                "        android:propertyName=\"rotationY\"\n" +
+//                "        android:interpolator=\"@android:anim/accelerate_decelerate_interpolator\"\n" +
+//                "        android:duration=\"300\" />\n" +
+//                "\n" +
+//                "    <!-- Half-way through the rotation (see startOffset), set the alpha to 1. -->\n" +
+//                "    <objectAnimator\n" +
+//                "        android:valueFrom=\"0.0\"\n" +
+//                "        android:valueTo=\"1.0\"\n" +
+//                "        android:propertyName=\"alpha\"\n" +
+//                "        android:startOffset=\"150\"\n" +
+//                "        android:duration=\"1\" />\n" +
+//                "</set>");
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                logHelper.e("Hey");
+//            }
+//        });
+//        thread.start();
+//
+//        LogUtil.methodCount(3).json("{\"name\":\"Leonardo Taehwan Kim\",\"email\":\"leonardo@thefinestartist.com\"}");
+//        LogUtil.e("Hello World");
+//
+//
+//        LogUtil.v("Hello World");
+//        LogUtil.tag("MainActivity").e(12.0f);
+//        LogUtil.methodCount(3).json("{\"name\":\"Leonardo Taehwan Kim\",\"email\":\"leonardo@thefinestartist.com\"}");
 
         LogUtil.v("Hello World");
         LogUtil.tag("MainActivity").e(12.0f);
         LogUtil.methodCount(3).json("{\"name\":\"Leonardo Taehwan Kim\",\"email\":\"leonardo@thefinestartist.com\"}");
+
+        logHelper.v("Hello World");
+        logHelper.e(12.0f);
+        logHelper.json("{\"name\":\"Leonardo Taehwan Kim\",\"email\":\"leonardo@thefinestartist.com\"}");
     }
 
     @Override
